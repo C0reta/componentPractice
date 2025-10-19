8126f364df66ef19ae6cd5621fdfea8c73bdcf07
C0reta <161186212+C0reta@users.noreply.github.com>
2025-10-20 00:47:26 +0900

    test2

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 var st_red={color:'red'}
 var st_bigBlue={color:'blue', fontWeight:'bold', fontSize:30}
 
 export default function App() {
   return (
-    <View style={{paddingTop: 50}}>
-      <Text style={st_red}>just red</Text>
-      <Text style={st_bigBlue}>just bigBlue</Text>
-      <Text style={[st_bigBlue, st_red]}>bigBlue, then red</Text>
-      <Text style={[st_red, st_bigBlue]}>bigBlue, then red 2</Text>
-      <Text style={[st_bigBlue, st_red, {fontSize: 20}]}>bigBlue, then red, then 20</Text>
+    <View style={{paddingTop: 50, alignItems:'baseline'}}>
+      <Text style={{fontSize:30, backgroundColor: 'cyan'}}>ABC</Text>
+      <Text style={{fontSize:40, backgroundColor: 'yellow'}}>ABC</Text>
+      <Text style={{fontSize:50, backgroundColor: 'lightgreen'}}>ABC</Text>
+      <Button title="Touch me"/>
     </View>
   );
 }
