145d9690771d5ae4817ea1dd21cdc9d162cc5c49
C0reta <161186212+C0reta@users.noreply.github.com>
2025-10-20 00:39:48 +0900

    test1

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+var st_red={color:'red'}
+var st_bigBlue={color:'blue', fontWeight:'bold', fontSize:30}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{paddingTop: 50}}>
+      <Text style={st_red}>just red</Text>
+      <Text style={st_bigBlue}>just bigBlue</Text>
+      <Text style={[st_bigBlue, st_red]}>bigBlue, then red</Text>
+      <Text style={[st_red, st_bigBlue]}>bigBlue, then red 2</Text>
+      <Text style={[st_bigBlue, st_red, {fontSize: 20}]}>bigBlue, then red, then 20</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
